c141a613062ab358a264605e0f3718bd3662d1d4
Andrii Terliuk <andrey.terlyuk@gmail.com>
2020-05-28 14:04:06 -0500

    Adding WFsim scripts

---
+++ WFsim_pattern_gen/WFsim_configs.py
@@ -0,0 +1,152 @@
+import numpy as np
+import wfsim
+import strax 
+
+class TrainingPatternS2WFsim(strax.LoopPlugin):
+    depends_on = ('peaks','peak_basics','truth')
+    provides = 'training_patterns_s2'
+    __version__ = '0.0.1'
+
+    dtype = [('time',np.int),    
+             ('endtime',np.int),
+             ('x',np.float),
+             ('y',np.float),
+             ('z',np.float),
+             ('npeaks', np.int), 
+             (("Integrated signal per PMT [ PE ]","area_per_channel"), np.float, (494,)), 
+             ("amp", np.float), 
+             ("n_electron", np.float),
+             ("area", np.float)
+            ]
+    def compute(self, peaks, truth):
+        #print("Types : " , truth['type'])
+        true_S2s = truth[truth['type']==2]
+        result = np.zeros(len(true_S2s), dtype = self.dtype)
+        for i, t in enumerate(true_S2s):
+            result['time'][i] = t['time']
+            result['endtime'][i] = t['endtime']
+            result['x'][i] = t['x']
+            result['y'][i] = t['y']
+            result['z'][i] = t['z']
+            result['amp'][i] = t['amp']
+            result['n_electron'] = t['n_electron']
+            p = peaks[((peaks['time'] < t['t_mean_photon'])&
+                       (peaks['endtime'] > t['t_mean_photon']))]
+            result['npeaks'][i] = len(p)
+            if len(p)==1:
+                result['area_per_channel'][i] = p["area_per_channel"]
+                result['area'][i] = p['area']
+            elif len(p)>1:
+                result['area_per_channel'][i] = p["area_per_channel"].sum(axis=0)
+                result['area'][i]  = p["area"].sum(axis=0)
+        return(result)
+
+class WFsim_config: 
+    """
+    This is a simple function that creates configuration string
+    and turns it into WFsim generator
+    :params 
+    - config - name of the configuration in format <A_configuration__Z_configuration>
+    - PRNG - random generator, standard NumPy will be used if no generator provided 
+    """
+    def __init__(self, config="", PRNG = None):
+        if PRNG==None:
+            print("INFO: no RNG is provided, using standard NumPy")
+            self.randgen = np.random
+        else: 
+            self.randgen = PRNG
+        self.Nevents = -1
+        self.confname = config
+        if config == "":
+            print("INFO: no config provided! Use SetAconf() and SetZconf() to set config")
+            self.confname = "__"
+        else:   
+            confA,confZ = self.confname.split("__")
+            print("Found config for amplitude : ",confA) 
+            print("Found confog for Z : ", confZ)
+            self.SetAconf(confA)
+            self.SetZconf(confZ)
+        
+    def SetAconf(self, confA):
+        confA_split = confA.split("_")
+        self.typeA = confA_split[1]
+        if self.typeA == "const": 
+            assert len(confA_split)==3, "wrong format for constant amplitude, example A_const_1000"
+            self.amp=float(confA_split[2])
+            print ("Will generate amplitude : ", self.amp, " electrons")
+            self.GenerateA = self.GenerateConstA
+        elif self.typeA == "lin":
+            assert len(confA_split)==4, "wrong format, use \"A_lin_min_max\", for example A_lin_100_1000"
+            self.minA = float(confA_split[2])
+            self.maxA = float(confA_split[3])
+            assert self.maxA >= self.minA, "maximal value should be larger than minimal"
+            print ("Will generate amplitude in range between", self.minA, "and", self.maxA)
+            self.GenerateA = self.GenerateLinA
+        else: 
+            raise ValueError("Unknown configuration for amplitude generation provided : "+confA)
+    def SetZconf(self, confZ):
+        
+        confZ_split = confZ.split("_")
+        self.typeZ = confZ_split[1]
+        if self.typeZ == "const": 
+            assert len(confZ_split)==3, "wrong format for constant Z, example Z_const_-5.0"
+            self.zvalue=float(confZ_split[2])
+            self.GenerateZ = self.GenerateConstZ
+            print ("Will generate Z = ", self.zvalue, " cm")
+        elif self.typeZ == "lin":
+            assert len(confZ_split)==4, "wrong format for Z range, use Z_lin_min_max"
+            self.minZ = float(confZ_split[2])
+            self.maxZ = float(confZ_split[3])
+            assert self.maxZ >= self.minZ, "maximal value should be larger than minimal!"
+            print ("Will generate Z in range between", self.minZ, "and", self.maxZ)
+            self.GenerateZ = self.GenerateLinZ
+        elif self.typeZ == "full":
+            assert len(confZ_split)==2, "too many arguments for full config "
+            print ("Will generate Z in full range")
+            self.GenerateZ = self.GenerateFullZ
+        else:
+            raise ValueError("Unknown configuration for Z generation provided : "+confZ)
+    def SetNumberOfEvents(self, nevt):
+        self.Nevents = nevt
+        print("Set number of events manually to : ", self.Nevents )
+    def SetManualInstruction(self, instruction):
+        self.instruction =  instruction
+    def GetManualInstruction(self,c, verbose = False ):
+        print("==== Instruction ========\n", self.instruction,"\n=============")
+        return(self.instruction)
+    def GenerateConstA(self, size, c=None):
+        return(self.amp*np.ones(size))
+    def GenerateLinA(self, size, c=None):
+        return(self.randgen.uniform(self.minA, self.maxA,size=size))
+    def GenerateConstZ(self, size, c=None):
+        return(self.zvalue*np.ones(size))  
+    def GenerateLinZ(self, size, c=None):
+        return(self.randgen.uniform(self.minZ, self.maxZ,size=size))
+    def GenerateFullZ(self, size, c=None):
+        return(self.randgen.uniform(- c['tpc_length'], -0.0,size=size))    
+    def GenerateS2(self, c):
+        if self.Nevents < 0:
+            n = c['nevents'] = c['event_rate'] * c['chunk_size'] * c['nchunk']
+            c['total_time'] = c['chunk_size'] * c['nchunk']    
+        else: 
+            n = self.Nevents
+            c['total_time'] = c['chunk_size'] * c['nchunk']    
+        #print("Generating" , n)    
+        #print("TPC radius: ",c['tpc_radius'])
+        instructions = np.zeros(n, dtype=wfsim.instruction_dtype)
+        instructions['event_number'] = np.arange(1,n+1)
+        instructions['time'] = 1e9*np.arange(0,n, dtype= float) +1e6
+        r = np.sqrt(self.randgen.uniform(0., (c['tpc_radius']-0.5)**2.0, n) ) ## not going all the way up to the edge
+        phi = self.randgen.uniform(-np.pi, np.pi,n)
+        instructions['x'] = r * np.cos(phi) 
+        instructions['y'] = r * np.sin(phi) 
+        instructions['z'] = self.GenerateZ(n,c) 
+        instructions['type'] = np.repeat([2], n)
+        instructions['amp'] = self.GenerateA(n,c)
+        instructions['recoil'] = np.repeat(np.array(["er"], dtype='<U2'), n)
+        #print(instructions)
+        #print( np.column_stack ( 
+        #                ( np.arange(1,len(instructions['x'])+1), 
+        #                  np.sqrt(instructions['x']**2 + instructions['y']**2 ))  ) 
+        #    )
+        return(instructions)
---
+++ WFsim_pattern_gen/generate_patterns.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+from optparse import OptionParser
+from os.path import expandvars
+import os, sys, random
+import numpy as np
+import strax, straxen, wfsim
+from time import time
+
+sys.path.append(os.path.dirname(__file__))
+from WFsim_configs import WFsim_config, TrainingPatternS2WFsim
+
+usage = "usage: %prog [options] inputfile"
+parser = OptionParser(usage)
+parser.add_option("-c", "--config", default=None, 
+                dest= "CONFIG", help = "Name of the configuration/run") 
+parser.add_option("-o", "--outfile", default = "test.hdf5", 
+                dest = "OUTFILE", help = "Name of the output file")
+parser.add_option("-r", "--runid", default = "1", type=int,
+                 dest = "RUNID", help = "Number of the run, should be integer")
+parser.add_option("-f", "--filenr", default = None, type=int,
+                 dest = "FILENR", help = "Number of the current file, should be integer")
+parser.add_option("-n", "--numevents", default = 1000, type=int,
+                 dest = "NUMEVENTS", help = "Number of events/patterns to generate")
+(options,args) = parser.parse_args()
+
+
+
+print("Using configuration : ", options.CONFIG)
+print("Using run id : " , options.RUNID)
+print("File number : " , options.FILENR)
+assert options.FILENR !=None , "File number in -f must be selected"
+assert options.FILENR >=0, "File number is smaller than 0!"
+seed = 21692380183192084+int(options.RUNID)*10000+int(options.FILENR) 
+run_id=str(options.RUNID)
+print("Events to generate : ", options.NUMEVENTS)
+print("Seed for RNG : ", seed)
+print("OUTFILE : ", options.OUTFILE)
+cur_RG =  np.random.Generator(np.random.PCG64(seed))
+print("Testing RNG : ", cur_RG.uniform(size=10))
+
+gen = WFsim_config(options.CONFIG,cur_RG)
+
+st = strax.Context(
+        storage=strax.DataDirectory('./strax_data'),
+        register=wfsim.RawRecordsFromFax,
+        config=dict(detector='XENONnT',
+                    to_pe_file= 'https://raw.githubusercontent.com/XENONnT/'
+                                   'strax_auxiliary_files/master/fax_files/to_pe_nt.npy',
+                    fax_config='https://raw.githubusercontent.com/XENONnT/'
+                               'strax_auxiliary_files/master/fax_files/fax_config_nt.json',
+                    **straxen.contexts.xnt_common_config,),
+        timeout= 3600,
+        **straxen.contexts.common_opts)
+
+config = dict(nchunk=1, event_rate=1, chunk_size=200)#options.NUMEVENTS,)
+st.set_config(config)
+st.set_config(dict(fax_file=None))
+
+gen.SetNumberOfEvents(options.NUMEVENTS)
+wfsim.strax_interface.rand_instructions = gen.GenerateS2
+
+start = time()
+truth = st.get_array(run_id, 'truth')
+
+#print("=======   Truth   ==========\n", truth)
+#truth_df = st.get_df(run_id, 'truth')
+raw_records = st.get_array(run_id,'raw_records')
+#records = st.get_array(run_id,'records')
+#peaks = st.get_array(run_id, "peaks")
+#peak_basics = st.get_array(run_id,'peak_basics')
+
+st.register(TrainingPatternS2WFsim)
+training_patterns_s2 = st.get_array(run_id,'training_patterns_s2')
+stop = time()
+# Now transforming the data
+raw_pos = np.zeros( (len(training_patterns_s2['x']),3) )
+raw_pos[:,0] = training_patterns_s2['x']
+raw_pos[:,1] = training_patterns_s2['y'] 
+raw_pos[:,2] = training_patterns_s2['z']
+
+corrected_pos = np.zeros( (len(training_patterns_s2['x']),3) )
+angle = 210*np.pi/180
+corrected_pos[:,0] =  training_patterns_s2['x']*np.cos(angle) + training_patterns_s2['y']*np.sin(angle) 
+corrected_pos[:,1] = -training_patterns_s2['x']*np.sin(angle) + training_patterns_s2['y']*np.cos(angle) 
+corrected_pos[:,2] = training_patterns_s2['z']
+
+area_per_channel = training_patterns_s2['area_per_channel']
+n_electron = training_patterns_s2['n_electron']
+amplitude = training_patterns_s2['amp']
+
+print("Total pattern to save: ", len(training_patterns_s2['x']))
+#
+
+
+#print("Raw pos", raw_pos)
+import h5py
+outfile = h5py.File(options.OUTFILE, "w")
+outfile.create_dataset("raw_pos", data = raw_pos, compression="gzip")
+outfile.create_dataset("corr_pos", data = corrected_pos, compression="gzip")
+outfile.create_dataset("area_per_channel", data = area_per_channel, compression="gzip")
+outfile.create_dataset("n_electron", data = n_electron, compression="gzip")
+outfile.create_dataset("amplitude", data = amplitude, compression="gzip")
+outfile.close()
+
+print("Total time to generate: ",stop-start, " s")
